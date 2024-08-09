33709aaf8020f8526b1e94c01f99fde773d1704a
Igor Polousov <igorpolousov@gmail.com>
2024-08-09 14:35:31 +0300

    Initial Commit

---
+++ SwiftUI_CoreData_24/SwiftUI_CoreData_24App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_CoreData_24App.swift
+//  SwiftUI_CoreData_24
+//
+//  Created by Igor Polousov on 09.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_CoreData_24App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
