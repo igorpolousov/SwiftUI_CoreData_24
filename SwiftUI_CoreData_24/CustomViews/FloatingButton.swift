049050bad7733e63a79f1334deedab07d09e0766
Igor Polousov <igorpolousov@gmail.com>
2024-08-28 16:51:58 +0300

    + images

---
+++ SwiftUI_CoreData_24/CustomViews/FloatingButton.swift
@@ -0,0 +1,18 @@
+//
+//  FloatingButton.swift
+//  SwiftUI_CoreData_24
+//
+//  Created by Igor Polousov on 28.08.2024.
+//
+
+import SwiftUI
+
+struct FloatingButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FloatingButton()
+}
